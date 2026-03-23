6d862abd65c334981c70ed4d792fb827b03d8583
Lakori4 <santivalde04@gmail.com>
2026-03-23 09:42:05 +0100

    favourites page

--- src/app/favoritos/page.tsx
+++ src/app/favoritos/page.tsx
@@ -1,15 +1,23 @@
+import { useMusic } from "@/context/MusicContext"
 
 
 
 const Favoritos = () => {
 
 
-    { favAl }
+    const { favAlbumsList, favAlbumsListPop } = useMusic();
+
 
 
     return (
         <div>
-
+            {favAlbumsList.map(a => (
+                <div className="albumCard">
+                    <img src={a.trackViewUrl} alt={a.trackName} />
+                    <h3>{a.trackName}</h3>
+                    <h4>{a.artistName}</h4>
+                </div>
+            ))}
         </div>
     )
 }
