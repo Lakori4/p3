96df34cac4da51e93161bc43ad7b97512e45f59c
eric <eric2004_24@hotmail.com>
2026-03-23 09:41:19 +0100

    album search page

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -2,6 +2,40 @@ import Link from "next/link";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { searchAlbum } from "@/lib/api";
+import { useEffect, useState } from "react";
+import { Album } from "@/lib/types";
 
 
-export default async function AlbumSearchPage ():
+export default async function AlbumSearchPage() {
+    const [Albums,setAlbums] = useState <Album[]>([]);
+    const [busqueda, setbusqueda] = useState ("");
+    const [search, setSearch]= useState<boolean>(false)
+
+    useEffect (()=>{
+
+        async function fetchAlbums() {
+            const query = busqueda.trim();
+            const data= searchAlbum(query)
+            setAlbums(await data)
+        }
+        fetchAlbums();
+
+    })
+    return (
+        <div>
+            <div className="searchContainer">
+                <input 
+                type="text" 
+                placeholder="Buscar un cocktail..." 
+                value={busqueda} 
+                onChange={(e) => setbusqueda(e.target.value)} 
+                className="searchInput"
+                />
+
+                <button className="searchButton" onClick={() => setSearch(!search)}>Buscar</button>
+            </div>
+
+        </div>
+        
+    )
+}
