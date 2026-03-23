6f64ed056222c2061e7744c4ab616057e6c069b0
Lakori4 <santivalde04@gmail.com>
2026-03-23 10:31:27 +0100

    fixed shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import "./page.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Pagina Principal</h1>
-
-      <div className="home">
+      <div>
         <Link href="/favoritos"><button type="button">Pagina favoritos</button></Link>
         <Link href="/albums"><button type="button">Pagina buscar</button></Link>
       </div>
