85c36adc4b4e866a1687d47c6240660af4649716
eric <eric2004_24@hotmail.com>
2026-03-23 09:28:26 +0100

    algunas cosiilas

---
+++ src/app/albums/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { searchAlbum } from "@/lib/api";
+
+
+export default async function AlbumSearchPage ():
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <div>
+      <h1>"Pagina Principal"</h1>
+
+      <div>
+        <button type="button">"Pagina favoritos"</button>
+        <button type="button">"Pagina buscar</button>
+      </div>
+
+    </div>
   );
 }
