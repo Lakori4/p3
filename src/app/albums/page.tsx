9a97a52a0590eb5c6da215566aacbddf12059755
Lakori4 <83789283+Lakori4@users.noreply.github.com>
2026-03-26 15:40:00 +0000

    added a timeout to search after 250 ms of not changing the input

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { searchAlbum } from "@/lib/api";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Album } from "@/lib/types";
 import AlbumCard from "@/components/AlbumCard";
 import "@/styles/Search.css";
@@ -11,17 +11,24 @@ export default function AlbumSearchPage() {
     const [Albums, setAlbums] = useState<Album[]>([]);
     const [busqueda, setbusqueda] = useState("");
     const [search, setSearch] = useState<boolean>(false)
+    const timeout = useRef<number>(0);
+
+    async function fetchAlbums() {
+        const query = busqueda.trim();
+        const data = await searchAlbum(query)
+        setAlbums(data)
+    }
 
     useEffect(() => {
 
-        async function fetchAlbums() {
-            const query = busqueda.trim();
-            const data = await searchAlbum(query)
-            setAlbums(data)
-        }
-        fetchAlbums();
+    if (timeout) {
+      clearTimeout(timeout.current);
+      timeout.current = setTimeout(fetchAlbums, 250)
+      setSearch(!search)
+      }
+    }, [busqueda])
 
-    }, [search])
+
     return (
         <div>
             <div className="searchContainer">
@@ -29,10 +36,11 @@ export default function AlbumSearchPage() {
                     type="text"
                     placeholder="Buscar un album..."
                     value={busqueda}
-                    onChange={(e) => setbusqueda(e.target.value)}
                     className="searchInput"
+                    onChange={(e) => setbusqueda(e.target.value)}
+                    /* onBlur={() => {setSearch(!search)}}
                     onKeyDown={e => { if (e.key === "Enter") { setSearch(!search) } }}
-                />
+             */    />
 
                 <button className="searchButton" onClick={() => setSearch(!search)}>Buscar</button>
             </div>
